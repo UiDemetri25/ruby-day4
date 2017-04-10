f3a11ef0fe34617ff09ac1c103efc4f1e9980f6b
Demetri Williams <demetriwilliams25@gmail.com>
2017-04-10 10:51:00 -0400

    paired students up

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,3 +1,13 @@
 students = ["Zari", "Tyree", "Mylea", "Hanna", "Justin"]
 
-puts "students"
+students.shuffle!
+# puts students
+
+count = 0
+
+num_students = students.length - 1
+
+until count == num_students
+	puts "#{students[count]} & #{students[count + 1]}"
+	count +=2
+end
